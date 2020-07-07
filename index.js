ed808b20ff1268fd9279c4011560cab3d2f1ccc5
chrou <chrou@grandstream.cn>
2020-07-07 20:05:53 +0800

    revert

--- index.js
+++ index.js
@@ -2,31 +2,20 @@ const images = ['fox1','fox2','fox3','fox4'];
 const imgElem = document.querySelector('img');
 
 function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
+    let randomNo =  Math.floor(Math.random() * array.length);
+    return array[randomNo];
 }
 
 setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
+    let randomChoice = randomValueFromArray(images);
+    imgElem.src = './images/' + randomChoice + '.jpg';
 }, 2000)
 
 // Register service worker to control making site work offline
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./sw.js', {scope: '/PWA_Demo'})
-            .then(function (registration) {
-                // 注册成功
-                console.warn('ServiceWorker registration successful with scope: ', registration.scope)
-            })
-            .catch(function (err) {
-                // 注册失败:(
-                console.error('ServiceWorker registration failed: ', err)
-            })
-    })
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(function() { console.log('Service Worker Registered'); });
 }
-
-
 
 // Code to handle install prompt on desktop
 
@@ -35,19 +24,16 @@ const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-console.warn("before install prompt")
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
+    console.warn("before install prompt")
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
     showInstallPromotion()
 });
 
 function showInstallPromotion(){
-    if( !deferredPrompt ) {
-        return;
-    }
     console.warn("show install promotion...")
     addBtn.style.display = 'block';
     addBtn.addEventListener('click', (e) => {
@@ -58,15 +44,12 @@ function showInstallPromotion(){
         // Wait for the user to respond to the prompt
         deferredPrompt.userChoice.then((choiceResult) => {
             if (choiceResult.outcome === 'accepted') {
-                alert('用户已同意添加到桌面');
+                console.log('User accepted the A2HS prompt');
             } else {
-                alert('用户已取消添加到桌面');
+                console.log('User dismissed the A2HS prompt');
             }
             deferredPrompt = null;
         });
     });
 }
 
-window.addEventListener('appinstalled', (evt) => {
-    alert('已安装到桌面屏幕');
-});
