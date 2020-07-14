45d831a9c04ebb465cc38512579235910a7c5ad6
chrou <chrou@grandstream.cn>
2020-07-14 14:51:53 +0800

    change video

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener('install', function (e) {
                 "./icon/fox-icon.png",
                 "./icon/like-152x152.png",
                 "./icon/like-512x512.png",
-                "./video/wanjuzongdongyuan.mp4",
+                "./video/yewen4.mp4",
                 './images/fox1.jpg',
                 './images/fox2.jpg',
                 './images/fox3.jpg',
